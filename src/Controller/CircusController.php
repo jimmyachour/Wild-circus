b3cea12d2cd7d0b92cadba804f4e43be5cfe29c7
Jimmy Achour <jimmyachour@hotmail.fr>
2019-07-16 16:41:26 +0200

    Home page

--- src/Controller/CircusController.php
+++ src/Controller/CircusController.php
@@ -5,13 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route(name="circus_")
+ */
 class CircusController extends AbstractController
 {
     /**
-     * @Route("/accueil", name="circus")
+     * @Route("/accueil", name="home")
      */
     public function index()
     {
-        return $this->render('circus/home.html.twig');
+        return $this->render('circus/home/index.html.twig');
+    }
+
+    /**
+     * @Route("/billeterie", name="ticket")
+     */
+    public function ticket()
+    {
+        return $this->render('circus/ticket/index.html.twig');
     }
 }
---
+++ src/Entity/Ticket.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TicketRepository")
+ */
+class Ticket
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+}
